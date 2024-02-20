27393b48f9430a441010a99cc9bb4ad07281e47f
meik1998news <mohammad+test@newsifier.com>
2024-02-20 22:05:03 +0300

    Handle time selection

--- app/components/UserMainCard.tsx
+++ app/components/UserMainCard.tsx
@@ -1,13 +1,18 @@
+"use client";
 import Image from "next/image";
 import React from "react";
+import { useGlobalContext } from "../lib/globalContext";
 
 export const UserMainCard = () => {
-  const timePeriods = ["Daily", "Weekly", "Monthly"];
+  const timePeriods = ["daily", "weekly", "monthly"];
+  const { selectedTime, setSelectedTime } = useGlobalContext();
+
+  const handleSelect = (time: string) => setSelectedTime(time);
 
   return (
     <div className="w-full h-full flex flex-col justify-start bg-dark-blue text-white rounded-b-xl">
-      <div className="h-60 bg-violet p-7 rounded-xl text-white flex flex-col items-start">
-        <div className="rounded-full border-2 w-14 h-14 border-white mb-7">
+      <div className="h-3/5 bg-violet p-7 rounded-xl text-white flex md:gap-6 gap-4 md:flex-col  md:items-start items-center">
+        <div className="rounded-full shrink-0 border-2 w-14 h-14 border-white md:mb-7">
           <Image
             width={500}
             height={500}
@@ -17,14 +22,21 @@ export const UserMainCard = () => {
           />
         </div>
 
-        <h3 className="text-white opacity-60 text-xs">Report for</h3>
-
-        <h1 className="text-2xl font-light">Mohamad Aleik</h1>
+        <div className="flex flex-col">
+          <h3 className="text-white opacity-60 text-xs">Report for</h3>
+          <h1 className="text-2xl font-light">Mohamad Aleik</h1>
+        </div>
       </div>
 
-      <div className="flex flex-col mt-auto py-5 gap-3 items-start px-7">
+      <div className="flex md:flex-col flex-row mt-auto py-5 gap-3 justify-between items-start px-7">
         {timePeriods.map((time) => (
-          <button className="text-sm hover:opacity-80" key={time}>
+          <button
+            onClick={() => handleSelect(time)}
+            className={`text-sm capitalize hover:opacity-80 transition-all 
+              ${selectedTime === time ? "opacity-100" : "opacity-60"}
+            `}
+            key={time}
+          >
             {time}
           </button>
         ))}
