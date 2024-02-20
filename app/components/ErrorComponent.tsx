cd45bcebe46afe0061df85ef99dbc3f48ce6384a
meik1998news <mohammad+test@newsifier.com>
2024-02-20 12:15:14 +0300

    Home page basic structure

---
+++ app/components/ErrorComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ErrorComponent = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-red-100 text-red-700">
+      <p className="text-xl font-semibold">Something went wrong...</p>
+    </main>
+  );
+};
