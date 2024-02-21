13cd7583dc063df18e25208d51bf57812f161e37
meik1998news <mohammad+test@newsifier.com>
2024-02-21 10:40:39 +0300

    Change color of main user card

--- app/components/UserMainCard.tsx
+++ app/components/UserMainCard.tsx
@@ -11,7 +11,7 @@ export const UserMainCard = () => {
 
   return (
     <div className="w-full h-full flex flex-col justify-start bg-dark-blue text-white rounded-b-xl">
-      <div className="h-3/5 bg-violet p-7 rounded-xl text-white flex md:gap-6 gap-4 md:flex-col  md:items-start items-center">
+      <div className="h-3/5 bg-blue p-7 rounded-xl text-white flex md:gap-0 gap-4 md:flex-col  md:items-start items-center">
         <div className="rounded-full shrink-0 border-2 w-14 h-14 border-white md:mb-7">
           <Image
             width={500}
